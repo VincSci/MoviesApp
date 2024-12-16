1f276783e37948080101782381bada3ac4cbdc36
VincSci <spyro.skyland@gmail.com>
2024-12-16 14:46:49 +0100

    Working search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3/search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  const fetchMovies = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`${BASE_URL}/movie?query=${query}`, {
+        params: {
+            api_key: API_KEY,
+        },
+    });
+      setMovies(response.data.results);
+    }
+    catch (err) {
+      console.log(err);
+      setError(err.message);
+    }
+    finally{
+      setLoading(false);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [query] )
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Movies App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e => setSearch(e.target.value)} />
+        <input type="submit" />
+      </form>
+      {error && <p>{error}</p>}
+      {loading && <p>Loading...</p>}
+      {movies && !loading && movies.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} />
+            <p>Note : {movie.vote_average}/10</p>
+          </div>
+        )
+      })}
     </>
   )
 }
 
-export default App
+export default App;
